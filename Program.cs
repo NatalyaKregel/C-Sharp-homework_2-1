10334f70e9b159616f56d73a5744573b5f24451e
Nata Kregel <natik.kregel@gmai.com>
2022-08-27 17:21:25 +0300

    Задание №1 к домашнему заданию

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10: Напишите программу, которая принимает 
+//на вход трёхзначное число и на выходе показывает 
+//вторую цифру этого числа.
+// 456 -> 5, 782 -> 8, 918 -> 1
+
+int SecondNum(int num)
+{
+    int x = num / 10;
+    x = x % 10;
+    return x;
+}
+Console.Write("Введите трехзначное число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = SecondNum(num);
+Console.WriteLine("Вторая цифра нашего числа = " + x);
